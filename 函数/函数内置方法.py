cc169bb5f43f6f5c3fc268439bb6894acd8e0f1a
gardenVan <1057013169@qq.com>
2018-06-02 22:34:09 +0800

    到pow()

--- 函数/函数内置方法.py
+++ 函数/函数内置方法.py
@@ -2,33 +2,55 @@
 # -*- coding: utf-8 -*-
 
 abs()
-#取绝对值
+# 取绝对值
 dict()
-#转换成字典
+# 转换成字典
 help()
-#帮助
+# 帮助
 min()
-#取最小值
+# 取最小值
 max()
-#去最大值
+# 去最大值
 all()
-#判断是True还是False，如果是可循环的数据集合(the iterable)是空(empty)的，就是True
+# 判断是True还是False，如果是可循环的数据集合(the iterable)是空(empty)的，就是True
 any()
-#如果这个可循环集合里有任何一个是True，就返回True
+# 如果这个可循环集合里有任何一个是True，就返回True
 dir()
-#打印了当前程序里承载的所有变量，
+# 打印了当前程序里承载的所有变量，
 hex()
-#转换成16进制
+# 转换成16进制
 slice()
-#切片，可以提前定义切片的规则，
+# 切片，可以提前定义切片的规则，
 divmod()
-#求两个值进去求他的整除，和余数,divmod(10,3)会返回(3,1)
+# 求两个值进去求他的整除，和余数,divmod(10,3)会返回(3,1)
 sorted()
-#sorted(iterable, /, *, key=None, reverse=False)可以通过这个方法，自定义一些排序的规则
+# sorted(iterable, /, *, key=None, reverse=False)可以通过这个方法，自定义一些排序的规则
 ascii()
-#返回Unicode编码
+# 返回Unicode编码
 enumerate()
-#enumerate()函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下表，一般用在for循环当中
+# enumerate()函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下表，一般用在for循环当中
 eval()
-#
+# 可以把一个字符串转成代码来执行,只能处理单行代码，有返回值
+exec()
+# 可以执行多行代码，和eval()用法一样，但是没有返回值
+ord()
+# 通过字符串返回ascii码的相应的数字值，
+chr()
+# 通过数字返回ascii对应的字符
+sum()
+# 把列表里的值求和
+bytearray()
+# bytearray() 方法返回一个新字节数组。这个数组里的元素是可变的，并且每个元素的值范围: 0 <= x < 256。
+map()
+# map() 会根据提供的函数对指定序列做映射。
+# 第一个参数 function 以参数序列中的每一个元素调用 function 函数，返回包含每次 function 函数返回值的新列表。
+filter()
+# 过滤附和条件的值，list(filter(lambda x:x>3 [1,2,3,4,5,6])),会返回[4,5,6]
+import functools
 
+functools.reduce()
+# map(),filter(),reduce()三剑客
+# reduce() 函数会对参数序列中元素进行累积。
+# 函数将一个数据集合（链表，元组等）中的所有数据进行下列操作：用传给 reduce 中的函数 function（有两个参数）先对集合中的第 1、2 个元素进行操作，得到的结果再与第三个数据用 function 函数运算，最后得到一个结果。
+pow()
+# 求多少次方
