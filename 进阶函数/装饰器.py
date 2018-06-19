0b8354abdd0ed38bdf14b80890185221246267dc
gardenVan <1057013169@qq.com>
2018-06-19 21:00:04 +0800

    重新设定项目内核
    重新设定项目内核

--- 进阶函数/装饰器.py
+++ 进阶函数/装饰器.py
@@ -6,47 +6,47 @@
 user_status = False
 
 
-def login(func):
-    def inner(*args,**kwargs):
-        global user_status
-        _username = 'garden'
-        _password = 'garden'
-        if (user_status == False):
-            username = input('user:').strip()
-            password = input('password:').strip()
-            if (username == _username and password == _password):
-                print('welcome!')
-                user_status = True
+def login(auth_type):
+    def outter(func):
+        def inner(*args, **kwargs):
+            global user_status
+            _username = 'garden'
+            _password = 'garden'
+            if (user_status == False):
+                username = input('user:').strip()
+                password = input('password:').strip()
+                if (username == _username and password == _password):
+                    print('welcome!')
+                    user_status = True
+                else:
+                    print('username or password wrong!')
             else:
-                print('username or password wrong!')
-        else:
-            print('用户已登录，验证通过!')
-        if user_status:
-            func(*args,**kwargs)
-
-    return inner
+                print('用户已登录，验证通过!')
+            if user_status:
+                func(*args, **kwargs)
+        return inner
+    return outter
 
 
-@login
+
 def Home():
     print('===首页===')
 
 
-@login
+
 def American():
     print('===欧美===')
 
 
-@login
+
 def China():
     print('===国产===')
 
-@login
+
+@login('qq')
 def Japanese(stely):
     print('===日本===', stely)
 
 
-Home()
+
 Japanese('3p')
-American()
-China()
